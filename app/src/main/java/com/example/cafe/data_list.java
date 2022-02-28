b9db00a3e3803554422ae0ba1be734700b97e2a6
achyutrimal4 <achyutrimal4@gmail.com>
2022-02-28 17:41:49 +0545

    2nd commit

---
+++ app/src/main/java/com/example/cafe/data_list.java
@@ -0,0 +1,14 @@
+package com.example.cafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class data_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_list);
+    }
+}

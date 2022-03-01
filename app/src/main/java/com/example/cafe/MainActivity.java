4ec8e1609f79ce2d3ffe098e5fbc0fc02f6f0c71
achyutrimal4 <achyutrimal4@gmail.com>
2022-03-01 17:19:58 +0545

    2nd commit

--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.cafe;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    private MaterialButton viewItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewItems = findViewById(R.id.viewItems1);
+        viewItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toDataList = new Intent(MainActivity.this, data_list.class);
+                startActivity(toDataList);
+            }
+        });
+
     }
 }
